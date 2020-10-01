5fe2404d0547d804580eea70783ec847ef0440cb
Oscar Romero <69870896+Manoban95@users.noreply.github.com>
2020-10-01 13:06:52 -0600

    Add files via upload

---
+++ userController.php
@@ -0,0 +1,29 @@
+<?php
+
+	include_once "config.php";
+	include_once "connectionController.php";
+
+	Class UserController
+	{
+		function get()
+		{
+			$con = connect();
+			if(!$con->connect_error){
+
+				$query = "select * from users";
+				$prepared_query = $con->prepare($query);
+				$prepared_query->execute();
+
+				$result = $prepared_query->get_result();
+				$users = $result->fetch_all(MYSQLI_ASSOC);
+
+				if ($users) {
+					return $users;
+					
+				}
+
+			}
+		}
+	}
+
+?>
